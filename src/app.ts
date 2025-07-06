474bfbeaa4e1b24e2c9c2c6f89db6646e1a4917a
pedrovjesus <pedroferreira161jesus@gmail.com>
2025-07-06 20:51:53 -0300

    refactor(app) #(06)

--- src/app.ts
+++ src/app.ts
@@ -3,40 +3,25 @@ import http, {
   IncomingMessage,
   ServerResponse,
 } from "http";
-
+import { handleRequest } from "./routes/route";
 class App {
   private server: HttpServer;
   private readonly port: number;
+
   constructor(port: number) {
     this.port = port;
     this.server = http.createServer(this.requestHandler.bind(this));
   }
 
   private requestHandler(req: IncomingMessage, res: ServerResponse): void {
-    if (req.url === "/") {
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "text/plain; charset=utf-8");
-      res.end("Olá do servidor TypeScript (com classes)!\n");
-    } else if (req.url === "/info") {
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "application/json");
-      res.end(
-        JSON.stringify({
-          message: "Esta é uma rota de informação!",
-          timestamp: new Date().toISOString(),
-        })
-      );
-    } else {
-      res.statusCode = 404;
-      res.setHeader("Content-Type", "text/plain; charset=utf-8");
-      res.end("Rota não encontrada.\n");
-    }
+    handleRequest(req, res);
   }
 
   public start(): void {
     this.server.listen(this.port, () => {
       console.log(`Servidor rodando em http://localhost:${this.port}/`);
     });
+
     process.on("SIGTERM", this.stop.bind(this));
     process.on("SIGINT", this.stop.bind(this));
   }
@@ -49,4 +34,4 @@ class App {
   }
 }
 
-export default App
+export default App;
