f55f9b0f88064656e266fcc320a35a3580fbd0eb
pedrovjesus <pedroferreira161jesus@gmail.com>
2025-07-06 20:45:20 -0300

    feat: created controller and routes

---
+++ src/controller/GenerationController.ts
@@ -0,0 +1,42 @@
+import { IncomingMessage, ServerResponse } from "http";
+import { FileGeneratorApp } from "../core/FileGeneratorApp";
+import path from "path";
+
+export class GenerationController {
+  private readonly templatesDir: string;
+  private readonly outputDir: string;
+  private readonly fileGenerator: FileGeneratorApp;
+
+  constructor() {
+    this.templatesDir = path.join(__dirname, "..", "templates");
+    this.outputDir = path.join(__dirname, "..", "..", "output");
+    this.fileGenerator = new FileGeneratorApp(
+      this.templatesDir,
+      this.outputDir
+    );
+  }
+
+  public async generate(req: IncomingMessage, res: ServerResponse) {
+    let body = "";
+
+    req.on("data", (chunk) => (body += chunk));
+
+    req.on("end", async () => {
+      try {
+        const json = JSON.parse(body);
+
+        if (!json.generationConfigs || !Array.isArray(json.generationConfigs)) {
+          throw new Error("generationConfigs invalid or missing.");
+        }
+
+        await this.fileGenerator.generate(json.generationConfigs);
+
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ message: "Files generated successfully!" }));
+      } catch (err: any) {
+        res.writeHead(500, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ error: err.message || "Unknown error" }));
+      }
+    });
+  }
+}
---
+++ src/routes/route.ts
@@ -0,0 +1,22 @@
+import { IncomingMessage, ServerResponse } from "http";
+import { GenerationController } from "../controller/GenerationController";
+
+const generationController = new GenerationController();
+
+export async function handleRequest(
+  req: IncomingMessage,
+  res: ServerResponse
+): Promise<void> {
+  if (req.method === "POST" && req.url === "/generate") {
+    return generationController.generate(req, res);
+  }
+
+  if (req.method === "GET" && req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Welcome to the file generation API!");
+    return;
+  }
+
+  res.writeHead(404, { "Content-Type": "application/json" });
+  res.end(JSON.stringify({ error: "Route not found" }));
+}
